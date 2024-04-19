c8c7e3cfedf724e8bde37195a0c32d4ff68a110c
Bnadraga <player.gamer1mw@gmail.com>
2024-04-19 22:20:13 +0300

    hw-1

---
+++ js/index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// const b = 10;
+// a = b * 2;
+// console.log(a);
+
+// console.log("hello goiteens".toLocaleUpperCase());
+
+// var b = 10;
+// var age = 13;
+
+// let age = 18;
+// age = 19;
+// console.log(age);
+
+// const b = true;
+// console.log(b);
+
+// let userName = false;
+// console.log(typeof userName)
+
+// const text = "Hello";
+// confirm(text);
+
+// const text = prompt("Умови погоджуєте?");
+// console.log(text);
