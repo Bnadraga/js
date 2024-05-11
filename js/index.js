e2472f5ce2005e4847b3a73349505aaa67a243b9
Bnadraga <player.gamer1mw@gmail.com>
2024-05-11 12:02:21 +0300

    ю

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 // const b = 10;
 // a = b * 2;
@@ -132,4 +132,111 @@
 
 // const sameMessage = `My name is ${name}, i'm ${age}!`;
 // console.log(sameMessage);
+
+// let value = 1;
+// let boolValue = Boolean(value);
+// console.log(boolValue)
+
+// const number = 15;
+// const result = number > 10 && number < 20;
+// console.log(result);
+
+// const number = 15;
+// const result = number > 16 && number < 20;
+// console.log(result);
+
+// const number = 15;
+// const full = number < 20 || number > 30;
+// console.log(full);
+
+// let a = 0;
+// const b = 'pro';
+// if (b === 'pro'){
+//     a = 100;
+// }
+// console.log('cost = ' + a);
+
+// let a = 0;
+// const b = "normal";
+// if (b === "pro") {
+//   a = 100;
+// }
+// else {
+//   a = -100;
+// }
+// console.log("a = " + a);
+
+// let a;
+// const b = 'premium'
+// if (b === 'normal'){
+//     a = 0;
+// }
+// else if (b === 'pro'){
+//     a = 100
+// }
+// else if (b === 'premium'){
+//     a = 400
+// }
+// else{
+//     console.log('not funded')
+// }
+// console.log('a = ' + a)
+
+// let a = prompt('Ваша підписка:');
+// let free = 'free';
+// let pro = 'pro';
+// let vip = 'vip';
+// if (a === pro || a === vip){
+//     console.log('Вхід дозволено')
+// }
+// else{
+//     console.log('Вхід не дозволено')
+// }
+
+// let type;
+// const age = 19;
+// if (age >= 18){
+//     type = 'adult'
+// }
+// else{
+//     type = 'child'
+// }
+// const age = 19;
+// const type = age >= 18 ? "adult" : "child";
+// console.log(type);
+
+// const num1 = 10;
+// const num2 = 20;
+// let bigger;
+// if (num1 > num2){
+//     bigger = num1;
+// }
+// else{
+//     bigger = num2;
+// }
+// const bigger = num1 > num2 ? num1 : num2;
+// console.log(bigger);
+
+// const input = prompt('Please, print your number');
+// if (input > 0){
+//     console.log('1')
+// }
+// else if (input < 0){
+//     console.log('-1')
+// }
+// else{
+//     console.log('0')
+// }
+
+// let result;
+// if (a + b < 4) {
+//   result = "Нижче";
+// } else {
+//   result = "Вище";
+// }
+
+// const a = 5;
+// const b = 2;
+// const result = (a + b < 4) ? 'Нижче' : 'Вище';
+// console.log(result)
 
